74500cd1a09929670307acbec01da7f030797674
zachary brewer <zenanbach@gmail.com>
2019-05-29 07:40:31 -0400

    Start Header and initialize gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zenanbach Creative`,
+    description: `Lean, creative, web development services`,
+    author: `@zenanbach`,
+    menuLinks:[
+      {
+          name:'home',
+          link:'/'
+      },
+      {
+          name:'page2',
+          link:'/page-2'
+      }
+    ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/zenanbach_icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import zenanbachLogo from '../images/zenanbach_logo.svg'
+
+const imgStyle = {
+  width: `90px`,
+  marginBottom: `0`
+}
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
+      borderBottom: `1px solid #000`
     }}
   >
     <div
@@ -16,17 +22,17 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#000`,
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <img style={imgStyle} src={zenanbachLogo} />
         </Link>
-      </h1>
+      
     </div>
   </header>
 )
