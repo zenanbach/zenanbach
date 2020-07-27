6b134499ee6e1f36cd27a836789584d94cf84be5
zachary brewer <zenanbach@gmail.com>
2020-07-27 07:07:06 -0400

    Add info to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,13 +9,18 @@ const About = () => (
     <h1>About</h1>
     <p>
       Zachary Brewer is a web engineer, musician, and father who splits his time
-      between Brooklyn, New York and the Western Catskills
+      between Brooklyn and the Western Catskills in New York State.
     </p>
     <p>
       Zach has been creating high quality, responsive web products since 2010. A
       musician by training, Zach's approach to information architecture is
       similar to their approach to music: boundless creativity within a strictly
       defined mathematical form.
+    </p>
+    <p>
+      Zach's primary focus is to work for organizations and companies that align
+      with his values of social responsibility, racial, social, and economic
+      justice, and sustainability.
     </p>
   </Layout>
 )
